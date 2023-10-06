7ff78e2bb410c8c7a14c3f06c889d9b8259951cf
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-06 12:38:21 +0100

    Added Meta class to Post model with get_latest_by, ordering and verbose_name

--- src/app_blog/models.py
+++ src/app_blog/models.py
@@ -16,6 +16,11 @@ class Post(models.Model):
         related_name="posts",
     )
 
+    class Meta:
+        get_latest_by = "published_date"
+        ordering = ["-published_date", "title"]
+        verbose_name = "blog post"
+
     def __str__(self) -> str:
         published_date_string = self.published_date.strftime("%Y-%m-%d")
         return f"{self.title}: published on {published_date_string}"
