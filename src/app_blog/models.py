3a0e7c9c8b10b0a264f63fe94dcf044e58a3362a
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-06 12:17:45 +0100

    defined organizer models

--- src/app_blog/models.py
+++ src/app_blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from app_organizer.models import Tag, StartUp
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    slug = models.CharField(max_length=64)
+    text = models.TextField()
+    published_date = models.DateField()
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="posts",
+    )
+    start_up = models.ManyToManyField(
+        StartUp,
+        related_name="posts",
+    )
