9212245d96680fc0521a2cee6ab413669b99931d
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-06 12:18:03 +0100

    define blog models; Post

--- src/app_organizer/models.py
+++ src/app_organizer/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField(
+        max_length=64,
+        unique=True,
+        help_text="A label for URL config",
+    )
+
+
+class StartUp(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(
+        max_length=64,
+        unique=True,
+        help_text="A label for URL config",
+    )
+    description = models.TextField()
+    founded_date = models.DateField()
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="start_ups",
+    )
+
+    class Meta:
+        indexes = [models.Index(fields=["name"])]
+
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=64)
+    slug = models.SlugField(
+        max_length=64,
+        help_text="A label for URL config",
+        unique=True,
+    )
+    publish_date = models.DateField()
+    link = models.URLField(max_length=255)
+    start_up = models.ForeignKey(
+        StartUp,
+        on_delete=models.CASCADE,
+        related_name="news_link",
+    )
