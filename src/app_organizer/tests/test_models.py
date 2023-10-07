1cc820e3f99c5d561e3cc533baecba6174ae4526
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-07 08:45:17 +0100

    Add create startup and check ordering by founded date

--- src/app_organizer/tests/test_models.py
+++ src/app_organizer/tests/test_models.py
@@ -1,6 +1,5 @@
 import pytest
-import json
-
+import datetime
 from django.test import TestCase
 from django.db.utils import IntegrityError
 from app_organizer.models import Tag, StartUp
@@ -10,16 +9,42 @@ from app_organizer.models import Tag, StartUp
 @pytest.mark.django_db
 class TestStartUpModel(TestCase):
     def test_create_startup(self):
-        some_tags = Tag.objects.bulk_create(
-            [Tag(name=name) for name in ["technology", "fintech"]]
-        )
-        StartUp.objects.create(
-            name="StartUp for tech name",
-            description="StartUp for tech description",
+        tag1, _ = Tag.objects.get_or_create(name="tag1")
+        tag2, _ = Tag.objects.get_or_create(name="tag2")
+        tag3, _ = Tag.objects.get_or_create(name="tag3")
+        tag4, _ = Tag.objects.get_or_create(name="tag4")
+
+        startup1 = StartUp.objects.create(
+            name="StartUp name 1",
+            description="StartUp description 1",
             founded_date="2015-12-10",
-            contact="tech@tech.com",
-            website="http://tech.tech.com",
-            tags=some_tags,
+            contact="startup1@tech.com",
+            website="http://startup1.tech.com",
+        )
+        startup2 = StartUp.objects.create(
+            name="StartUp name 2",
+            description="StartUp description 2",
+            founded_date="2019-12-10",
+            contact="startup2@tech.com",
+            website="http://startup2.tech.com",
+        )
+        startup3 = StartUp.objects.create(
+            name="StartUp name 3",
+            description="StartUp description 3",
+            founded_date="2014-01-01",
+            contact="startup3@tech.com",
+            website="http://startup3.tech.com",
+        )
+        startup1.tags.add(tag1, tag2)
+        startup2.tags.add(tag3, tag2)
+        startup3.tags.add(tag4, tag4, tag1)
+
+    def test_startup_ordered_by_founded_date(self):
+        self.test_create_startup()
+        TestCase.assertEqual(
+            self,
+            StartUp.objects.first().founded_date,
+            datetime.date(2019, 12, 10),
         )
 
 
