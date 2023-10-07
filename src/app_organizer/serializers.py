57b8f87d7719ac39cfd47f2fd3e13cf2e3ca02d9
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-07 22:35:41 +0100

    Added Organizer serializer

---
+++ src/app_organizer/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework.serializers import ModelSerializer, HyperlinkedRelatedField, Field
+
+from app_organizer.models import StartUp, Tag
+
+
+class TagSerializer(ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+
+class StartUpSerializer(ModelSerializer):
+    # tags = TagSerializer(many=True)
+
+    class Meta:
+        model = StartUp
+        fields = "__all__"
