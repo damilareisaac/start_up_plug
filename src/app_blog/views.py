fc46cae313b1765ce594d85976adee5b39205aac
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-08 16:17:39 +0100

    Added blog urls to global urls

--- src/app_blog/views.py
+++ src/app_blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def create_post(request):
+    ...
+
+
+def list_posts(request):
+    ...
+
+
+def post_detail(request):
+    ...
+
+
+def update_post(request):
+    ...
+
+
+def delete_post(request):
+    ...
--- src/config/urls.py
+++ src/config/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from app_blog import urls as app_blog_urls
 
-api_urls = app_blog_urls
+# api_urls = app_blog_urls
 
 urlpatterns = [
-    path(
-        path("api/v1/", include(api_urls)),
-    )
+    path("api/v1/blog/", include(app_blog_urls)),
     # path("admin/", admin.site.urls),
 ]
