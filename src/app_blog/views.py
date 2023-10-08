46299ae8d51a956f5b2d0cdba4d86c8625b88925
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-08 18:00:18 +0100

    Created post create API endpoint

--- src/app_blog/views.py
+++ src/app_blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from django.views.decorators import require_http_method
+from app_blog.serializers import PostSerializer
+from rest_framework.response import HttpResponse
 
 # Create your views here.
+
+
+@require_http_method(["POST"])
 def create_post(request):
-    ...
+    post = PostSerializer(request.data)
+    return HttpResponse(post.data)
 
 
 def list_posts(request):
