e98ea259505f5e9f7d7eed744aa9bd3515fd4178
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-08 15:57:41 +0100

    Added urls for App_blog

---
+++ src/app_blog/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+app_name = "app_blog"
+
+POST_DETAILS = "<int:year>/<int:month>/<int:day>"
+urlpatterns = [
+    path("", views.list_post, name="list_posts"),
+    path("create/", views.create_post, name="create_post"),
+    path(
+        POST_DETAILS.format("/"),
+        views.post_detail,
+        name="post_detail",
+    ),
+    path(
+        POST_DETAILS.format("/update/"),
+        views.update_post,
+        name="update_post",
+    ),
+    path(
+        POST_DETAILS.format("/delete/"),
+        views.delete_post,
+        name="delete_post",
+    ),
+]
