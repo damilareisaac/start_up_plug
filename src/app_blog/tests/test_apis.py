8583aaa320de90688feff9d4bdb18639b7f1b6ad
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-09 14:41:00 +0100

    Unfinished app blog views and api tests

--- src/app_blog/tests/test_apis.py
+++ src/app_blog/tests/test_apis.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
+import pytest
 
-# Create your tests here.
+
+END_POINT = "/api/v1/blog/{}/"
+
+
+@pytest.mark.django_db
+class TestPostCreateAPI(TestCase):
+    @pytest.mark.skip("not working yet")
+    def test_post_create(self):
+        request = RequestFactory()
+        post_data = {
+            "title": "Test to test what",
+        }
+        response = request.get(END_POINT.format("create"))
+        TestCase.assertEqual(self, response.status_code, 201)
--- src/app_blog/views.py
+++ src/app_blog/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render
-from django.views.decorators import require_http_method
+from app_blog.models import Post
+
+# from django.views.decorators import require_http_method
 from app_blog.serializers import PostSerializer
-from rest_framework.response import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 
 
-@require_http_method(["POST"])
+@api_view(["post"])
 def create_post(request):
-    post = PostSerializer(request.data)
-    return HttpResponse(post.data)
+    s_post = PostSerializer(request.data)
+    if s_post.is_valid():
+        return Response(s_post.data, status.HTTP_201_CREATED)
+    return Response(s_post.errors, status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(["get"])
 def list_posts(request):
-    ...
+    posts = Post.objects.all()
+    s_posts = PostSerializer(posts, many=True)
+    return Response(s_posts.data, status.HTTP_200_OK)
 
 
 def post_detail(request):
