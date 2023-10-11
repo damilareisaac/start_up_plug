88504a39d0df9fc7bb4336b67089bade4fa039ee
Isaac Omolewa <isaacomolewa@Isaacs-MacBook-Air-2.local>
2023-10-11 15:01:12 +0100

    Added create on startup serializer to create startup with and without tags

--- src/app_startup/serializers.py
+++ src/app_startup/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 
 from app_startup.models import StartUp
+from app_tag.models import Tag
 from app_tag.serializers import TagSerializer
 
 
 class StartUpSerializer(ModelSerializer):
-    tags = TagSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True)
 
     class Meta:
         model = StartUp
         fields = "__all__"
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop("tags")
+        startup = StartUp.objects.create(**validated_data)
+        if tags_data:
+            tags_list = [Tag.objects.get_or_create(**tag) for tag, _ in tags_data]
+            startup.tags.add(tags_list)
--- src/app_startup/tests/test_serializers.py
+++ src/app_startup/tests/test_serializers.py
@@ -6,7 +6,7 @@ from app_startup.serializers import StartUpSerializer
 
 @pytest.mark.django_db
 class TestStartUpSerializer(TestCase):
-    def test_start_up_serializer_with_no_tags(self):
+    def test_create_startup_with_no_tags(self):
         data = {
             "name": "StartUp name 1",
             "slug": "startup-name-1",
@@ -14,6 +14,30 @@ class TestStartUpSerializer(TestCase):
             "founded_date": "2015-12-10",
             "contact": "startup1@tech.com",
             "website": "http://startup1.tech.com",
+            "tags": [],
+        }
+        fake_request = RequestFactory().get("some_fake_endpoint")
+        s_startup = StartUpSerializer(
+            data=data,
+            context={"request": fake_request},
+        )
+        TestCase.assertTrue(self, s_startup.is_valid())
+        TestCase.assertEqual(self, s_startup.errors, {})
+        TestCase.assertDictEqual(self, s_startup.data, data)
+
+    def test_create_startup_with_tags(self):
+        data = {
+            "name": "StartUp name 1",
+            "slug": "startup-name-1",
+            "description": "StartUp description 1",
+            "founded_date": "2015-12-10",
+            "contact": "startup1@tech.com",
+            "website": "http://startup1.tech.com",
+            "tags": [
+                {"name": "tag1", "slug": "tag1"},
+                {"name": "tag2", "slug": "tag2"},
+                {"name": "tag3", "slug": "tag3"},
+            ],
         }
         fake_request = RequestFactory().get("some_fake_endpoint")
         s_startup = StartUpSerializer(
